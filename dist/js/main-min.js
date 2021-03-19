7dcee8873ba18ef82f120f016687a9fdafc7778e
abdelmonem88 <abdelmonem1888@gmail.com>
2021-03-19 21:01:00 +0200

    first commit

--- dist/js/main-min.js
+++
@@ -1,1 +0,0 @@
-const one=1;document.querySelector(".parent").style.backgroundColor="blue";const two=2;
--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,23 @@
 const gulp = require("gulp"),
-  concat = require("gulp-concat"),
-  autoprefixer = require("gulp-autoprefixer"),
-  sass = require("gulp-sass"),
-  pug = require("gulp-pug");
-livereload = require("gulp-livereload");
+ concat = require("gulp-concat"),
+ autoprefixer = require("gulp-autoprefixer"),
+ sass = require("gulp-sass"),
 sourcemaps = require("gulp-sourcemaps");
-minify = require("gulp-minify");
-notify = require("gulp-notify");
-
-//HTML Task
-gulp.task("html", function () {
-  return gulp
-    .src("stage/html/*.pug")
-    .pipe(pug({ pretty: true }))
-    .pipe(gulp.dest("dist"))
-    .pipe(livereload({ start: true }));
-});
 
 //css task
 gulp.task("css", function () {
-  return gulp
-    .src(["stage/css/**/*.css", "stage/css/**/*.scss"])
-    .pipe(sourcemaps.init())
-    .pipe(sass({ outputStyle: "compressed" }))
-    .pipe(autoprefixer("last 2 versions"))
-    .pipe(concat("main.css"))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("dist/css"))
-    .pipe(livereload({ start: true }));
-});
-
-//JS task
-gulp.task("js", function () {
-  return gulp
-    .src("stage/js/*.js")
-    .pipe(concat("main.js"))
-    .pipe(minify())
-    .pipe(gulp.dest("dist/js"))
-    .pipe(livereload({ start: true }));
+ return gulp
+  .src(["stage/css/**/*.css", "stage/css/**/*.scss"])
+  .pipe(sourcemaps.init())
+  .pipe(sass({ outputStyle: "compressed" }))
+  .pipe(autoprefixer("last 2 versions"))
+  .pipe(concat("main.css"))
+  .pipe(sourcemaps.write("."))
+  .pipe(gulp.dest("dist/css"))
 });
 
 //Watch Tasks
 gulp.task("watch", function () {
-  require("./server.js");
-  livereload.listen();
-  gulp.watch("stage/html/**/*.pug", gulp.series("html"));
-  gulp.watch(["stage/css/**/*.css", "stage/css/**/*.scss"], gulp.series("css"));
-  gulp.watch("stage/js/*.js", gulp.series("js"));
+ require("./server.js");
+ gulp.watch(["stage/css/**/*.css", "stage/css/**/*.scss"], gulp.series("css"));
 });
